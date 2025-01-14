c5d65e32cbac4a7f1e6db2db8fae827a0313a795
Jose Salcedo <Jsalcedo19@gmail.com>
2025-01-14 10:36:22 -0500

    add Feeding model, serializers, and views; update admin and URLs

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 # import Home view from the views file
-from .views import Home, CatList, CatDetail
+from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail
 
 urlpatterns = [
-  path('', Home.as_view(), name='home'),
+    path('', Home.as_view(), name='home'),
   # new routes below 
-  path('cats/', CatList.as_view(), name='cat-list'),
-  path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
+    path('cats/', CatList.as_view(), name='cat-list'),
+    path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
+    path('cats/<int:cat_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
+    path('cats/<int:cat_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
+
 ]
 

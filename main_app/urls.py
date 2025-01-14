0f40c8cd03db62984d0b6c7c60fde835b8a62b5a
Jose Salcedo <Jsalcedo19@gmail.com>
2025-01-14 09:27:35 -0500

    add Cat model, views, and serializers; register in admin and update URLs and random migrations

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 # import Home view from the views file
-from .views import Home
+from .views import Home, CatList, CatDetail
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  # new routes below 
+  path('cats/', CatList.as_view(), name='cat-list'),
+  path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
 ]
 
